846d26de1c096668738f161f81d878ac2b050053
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-05-17 20:56:55 +0200

    create db:seed and controler routes pages

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: {minimum: 6}
+  validates :movie_id, uniqueness: {scope: :list_id, message: 'Movie is already in the list'}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
